935cb66f7835a945895cefa01b0795bfb08fda95
Julia Oliveira <julia.oliveira.silva@outlook.com>
2020-07-21 10:26:36 -0300

    remove desafioescola

--- desafioescola/public/scripts.js
+++
@@ -1,9 +0,0 @@
-const modalOverlay = document.querySelector('.modal-overlay')
-const cards = document.querySelectorAll('.card')
-
-for (let card of cards) {
-    card.addEventListener("click", function() {
-        const pageId = card.getAttribute("id")
-        window.location.href = `/courses/${pageId}`
-    })
-}
